c3cfa749cdb8c53f37a3f6f5ba8ca82bd64385ce
viziadam <113590667+viziadam@users.noreply.github.com>
2026-07-10 18:28:50 +0200

    Localize auth default settings

--- wp-content/plugins/agency-module-auth/includes/services/settings.php
+++ wp-content/plugins/agency-module-auth/includes/services/settings.php
@@ -9,17 +9,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function agency_auth_defaults() {
 	return array(
-		'registration_enabled'     => 1,
-		'require_verification'     => 1,
-		'token_lifetime'           => 86400,
-		'login_redirect'           => home_url( '/fiokom/' ),
-		'account_page'             => 0,
-		'page_ids'                 => array(),
+		'registration_enabled'       => 1,
+		'require_verification'       => 1,
+		'token_lifetime'             => 86400,
+		'login_redirect'             => home_url( '/fiokom/' ),
+		'account_page'               => 0,
+		'page_ids'                   => array(),
 		'auto_create_required_pages' => 1,
-		'auto_add_auth_links'      => 1,
-		'auto_add_pages_to_menu'   => 0,
-		'email_subject'            => __( 'Verify your account', 'agency-module-auth' ),
-		'email_body'               => __( '<p>Welcome! Verify your email address here: {{verification_url}}</p>', 'agency-module-auth' ),
+		'auto_add_auth_links'        => 1,
+		'auto_add_pages_to_menu'     => 0,
+		'email_subject'              => __( 'Fiók megerősítése', 'agency-module-auth' ),
+		'email_body'                 => __( '<p>Üdvözlünk! Az email címedet itt tudod megerősíteni: {{verification_url}}</p>', 'agency-module-auth' ),
 	);
 }
 
