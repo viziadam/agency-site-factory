f50266b955c6a80a459add11e926476c15e4faf3
viziadam <113590667+viziadam@users.noreply.github.com>
2026-07-10 18:29:21 +0200

    Localize split header CTA

--- wp-content/themes/agency-theme/template-parts/header/header-split.php
+++ wp-content/themes/agency-theme/template-parts/header/header-split.php
@@ -16,7 +16,7 @@ $booking_url = function_exists( 'agency_core_get_setting' ) ? agency_core_get_se
 				<a class="agency-brand__name" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( agency_theme_brand_name() ); ?></a>
 			<?php endif; ?>
 		</div>
-		<nav class="agency-navigation" aria-label="<?php esc_attr_e( 'Primary navigation', 'agency-theme' ); ?>">
+		<nav class="agency-navigation" aria-label="<?php esc_attr_e( 'Elsődleges navigáció', 'agency-theme' ); ?>">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'agency-menu', 'fallback_cb' => false, 'depth' => 1 ) ); ?>
 		</nav>
 		<?php
@@ -25,8 +25,7 @@ $booking_url = function_exists( 'agency_core_get_setting' ) ? agency_core_get_se
 		}
 		?>
 		<?php if ( $booking_url ) : ?>
-			<a class="agency-button agency-button--primary agency-header-cta" href="<?php echo esc_url( $booking_url ); ?>"><?php esc_html_e( 'Book now', 'agency-theme' ); ?></a>
+			<a class="agency-button agency-button--primary agency-header-cta" href="<?php echo esc_url( $booking_url ); ?>"><?php esc_html_e( 'Időpontfoglalás', 'agency-theme' ); ?></a>
 		<?php endif; ?>
 	</div>
 </header>
-
